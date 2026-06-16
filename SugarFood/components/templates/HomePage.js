5b2d8b6b66632aae7ecc70a9d0b368d0dbc492d5
Sarvenaz Vazirifar <vazirifar.sarvenaz.1380@gmail.com>
2026-06-16 22:32:46 +0330

    feat: companies - Instruction - Guide - Restriction Components added to Home page

--- SugarFood/components/templates/HomePage.js
+++ SugarFood/components/templates/HomePage.js
@@ -1,6 +1,10 @@
 import Attributes from "../modules/Attributes";
 import Banner from "../modules/Banner";
+import Companies from "../modules/Companies";
 import Definition from "../modules/Definition";
+import Guide from "../modules/Guide";
+import Instruction from "../modules/Instruction";
+import Restrictions from "../modules/Restrictions";
 import styles from "./HomePage.module.css";
 
 function HomePage() {
@@ -8,7 +12,11 @@ function HomePage() {
     <div className={styles.container}>
       <Banner />
       <Attributes />
-      <Definition/>
+      <Definition />
+      <Companies />
+      <Instruction />
+      <Guide />
+      <Restrictions/>
     </div>
   );
 }
