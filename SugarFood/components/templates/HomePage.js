9df3cbf1b82ad5222f1765893ba160da6f2303a2
Sarvenaz Vazirifar <vazirifar.sarvenaz.1380@gmail.com>
2026-06-16 22:16:20 +0330

    feat: Banner - Attributes - Definition Components added to Home page

---
+++ SugarFood/components/templates/HomePage.js
@@ -0,0 +1,16 @@
+import Attributes from "../modules/Attributes";
+import Banner from "../modules/Banner";
+import Definition from "../modules/Definition";
+import styles from "./HomePage.module.css";
+
+function HomePage() {
+  return (
+    <div className={styles.container}>
+      <Banner />
+      <Attributes />
+      <Definition/>
+    </div>
+  );
+}
+
+export default HomePage;
