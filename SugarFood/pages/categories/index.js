1f1fd94af5fc7ec69b1146bcac69bcbbe6027de3
Sarvenaz Vazirifar <vazirifar.sarvenaz.1380@gmail.com>
2026-06-17 17:31:17 +0330

    refactor:URL changed

--- SugarFood/pages/categories/index.js
+++ SugarFood/pages/categories/index.js
@@ -11,7 +11,7 @@ export async function getServerSideProps(context) {
   const { query } = context;
   const { difficulty, time } = query;
 
-  const res = await fetch("http://localhost:4000/data");
+  const res = await fetch(`${process.env.BASE_URL}/data`);
   const data = await res.json();
 
   const filteredData = data.filter((item) => {
